e378669a90e847f20bc4a0724c4af3129917426a
HariniPamukuntla <harinipamukuntla@gmail.com>
2026-09-09 20:55:11 +0530

    Register optional web search context plugin

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,13 +3,14 @@ import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 import { groqApiPlugin } from './server/groqApi';
 import { sqliteApiPlugin } from './server/sqliteApi';
+import { webSearchApiPlugin } from './server/webSearchApi';
 
 const previewPort = Number(process.env.PORT) || 5000;
 
 export default defineConfig(({ mode }) => {
   Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
   return {
-    plugins: [groqApiPlugin(), sqliteApiPlugin(), react()],
+    plugins: [groqApiPlugin(), sqliteApiPlugin(), webSearchApiPlugin(), react()],
     server: { host: '0.0.0.0', port: previewPort, strictPort: true, allowedHosts: true },
     preview: { host: '0.0.0.0', port: previewPort, strictPort: true, allowedHosts: true },
     resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
